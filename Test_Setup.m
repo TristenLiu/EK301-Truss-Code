62fd02a8b010a478ba30daa5e6e5c336e5025208
trist <trist@DESKTOP-GH3OJIM>
2022-04-02 21:35:31 -0400

    Test Setup code for Practice Matrix, add analysis function when finished

---
+++ Test_Setup.m
@@ -0,0 +1,49 @@
+%Connection Matrix setup
+%The rows j represent joints, and columns m represent the members
+%For example, if (1,1) = 1, then joint 1 is connected to member 1 and 0
+%otherwise.
+C = [ [1 1 0 0 0 0 0 0 0 0 0 0 0];
+      [1 0 1 1 0 0 0 0 0 0 0 0 0];
+      [0 0 0 1 1 0 0 1 0 0 0 0 0];
+      [0 1 1 0 1 1 1 0 0 0 0 0 0];
+      [0 0 0 0 0 1 1 1 1 1 0 0 0];
+      [0 0 0 0 0 0 1 0 0 1 1 1 0];
+      [0 0 0 0 0 0 0 0 1 0 1 0 1];
+      [0 0 0 0 0 0 0 0 0 0 0 1 1] ];
+
+[joints, members] = size(C);
+
+%X Y Coordinates for the location of each joint
+X = [0 0 4 4 8 8 12 12];
+Y = [0 4 8 4 8 4 4 0];
+
+%Construction matrix for the support forces
+%Column 1 represents the horizontal reaction force at pin 1
+%A value of 1 in Sx(1,1) means at pin 1 there is a horizonal reaction F
+%A value of 1 in Sy(1,2) means at pin 1 there is a vertical reaction F
+Sx1 = zeros(joints, 1); 
+Sy1 = zeros(joints, 1); 
+Sy2 = zeros(joints, 1);
+
+Sx = [Sx1 Sy1 Sy2]; Sy = [Sx1 Sy1 Sy2];
+Sx(1,1) = 1; Sy(1,2) = 1; Sy(8,3) = 1;
+
+%Defining the Load vector L
+%The first j columns represent the Horizontal Load force at the j'th pin
+%The next j columns represent the Vertial Load force at the j'th pin
+L = zeros(2*joints, 1);
+
+L(1,4) = 25;         %There is a 25N load at pin 4 in the practice problem
+
+
+
+
+
+
+
+
+
+
+
+
+
