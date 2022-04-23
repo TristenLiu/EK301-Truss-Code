6d615bea6f49e8d648e6ec68644d74fcd9693ea4
TristenLiu <98703889+TristenLiu@users.noreply.github.com>
2022-04-23 13:28:42 -0400

    Final design

---
+++ TrussFinalDesign.m
@@ -0,0 +1,54 @@
+%Connection Matrix setup
+%The rows j represent joints, and columns m represent the members
+%For example, if (1,1) = 1, then joint 1 is connected to member 1 and 0
+%otherwise.
+C = [ [1 0 0 1 0 0 0 0 0 0 0 0 0];
+      [1 1 0 0 1 1 0 0 0 0 0 0 0];
+      [0 1 1 0 0 0 1 1 1 0 0 0 0];
+      [0 0 1 0 0 0 0 0 0 1 0 0 0];
+      [0 0 0 1 1 0 0 0 0 0 1 0 0];
+      [0 0 0 0 0 1 1 0 0 0 1 1 0];
+      [0 0 0 0 0 0 0 1 0 0 0 1 1];
+      [0 0 0 0 0 0 0 0 1 1 0 0 1] ];
+
+[joints, members] = size(C);
+
+%X Y Coordinates for the location of each joint
+X = [0 11 20 32 5.5 13.86 21.88 29.97];
+Y = [0 0 0 0 6.48 8 10.84 8.25];
+
+%Construction matrix for the support forces
+%Column 1 represents the horizontal reaction force at pin 1
+%A value of 1 in Sx(1,1) means at pin 1 there is a horizonal reaction F
+%A value of 1 in Sy(1,2) means at pin 1 there is a vertical reaction F
+Sx1 = zeros(joints, 1); 
+Sy1 = zeros(joints, 1); 
+Sy2 = zeros(joints, 1);
+
+Sx = [Sx1 Sy1 Sy2]; Sy = [Sx1 Sy1 Sy2];
+Sx(1,1) = 1; Sy(1,2) = 1; Sy(4,3) = 1;
+
+%Defining the Load vector L
+%The first j columns represent the Horizontal Load force at the j'th pin
+%The last j columns represent the Vertial Load force at the j'th pin
+L = zeros(2*joints, 1);
+
+L(joints+3,1) = 25;         %There is a 25N vertical load at pin 4 in the practice problem
+
+%Testing validity of truss
+if (members ~= 2*joints-3)
+    fprintf('Error! Please check your contruction matrix for mistakes\n');
+else
+    save('FinalTruss.mat','C','Sx','Sy','X','Y','L');
+end
+
+
+
+
+
+
+
+
+
+
+
